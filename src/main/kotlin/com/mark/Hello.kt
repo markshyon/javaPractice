209af837092b6b131579cbac97fcabc998127e6b
mark <hideen@gmail.com>
2018-12-19 21:57:53 +0800

    Initial commit

---
+++ src/main/kotlin/com/mark/Hello.kt
@@ -0,0 +1,14 @@
+package com.mark
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin")
+    }
+}
